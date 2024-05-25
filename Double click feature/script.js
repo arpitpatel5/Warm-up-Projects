1bf21ec68e8987ce23e5747867ef289b70164fb6
arpitpatel5 <arpit.patel5005@gmail.com>
2024-05-25 19:29:03 +0530

    double click for alerts

---
+++ Double click feature/script.js
@@ -0,0 +1,6 @@
+//call HTML element
+const boxelement=document.getElementById("box");
+//action perfomred on element 
+boxelement.addEventListener("dblclick", function(){
+    alert("Your double click is working properly, Happy coding😁");
+})
