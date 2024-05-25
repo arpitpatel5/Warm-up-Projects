9b989154a799875764ce74b88d482fbd2fc0f024
arpitpatel5 <arpit.patel5005@gmail.com>
2024-05-25 19:37:46 +0530

    Mouseout using DOM

---
+++ Mouse out feature/script.js
@@ -0,0 +1,6 @@
+//call html element
+const boxelement = document.getElementById("box");
+//add event listener
+boxelement.addEventListener("mouseout", function(){
+    alert("You left the safe zone man🙆‍♂️(mouseout working")
+})
